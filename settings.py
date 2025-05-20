f5c566dca8f7f640bd3466857fec850bb7ac417a
rahul-0430 <rahulpolicepatil0430@gmail.com>
2025-05-20 23:29:15 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hxp5k7vnircc!46s0_kkee#5t1_byxc!wrrh*&0j5qjb()negd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
